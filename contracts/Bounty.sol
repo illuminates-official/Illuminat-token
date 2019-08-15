43739ee8239eaffe8181e63897e2f32bc088dfc6
d3c6e1 <p0nede1niq@gmail.com>
2019-08-15 14:33:59 +0300

    add tests for bounty contract

--- contracts/Bounty.sol
+++ contracts/Bounty.sol
@@ -32,7 +32,7 @@ contract Bounty is Ownable {
     }
 
     function addAirdropAccounts(address[] memory _account, uint[] memory _balance) public onlyOwner {
-        require(_account.length == _balance.length);
+        require(_account.length == _balance.length, "The length of the arrays must be equal");
 
         for (uint i = 0; i < _account.length; i++) {
             _addAirdropAccount(_account[i], _balance[i]);
@@ -40,6 +40,7 @@ contract Bounty is Ownable {
     }
 
     function receiveTokens() public {
+        require(!airdropReceived[msg.sender], "Airdrop tokens already received");
         uint amount = airdropBalances[msg.sender];
         airdropBalances[msg.sender] = 0;
         airdropReceived[msg.sender] = true;
@@ -56,9 +57,9 @@ contract Bounty is Ownable {
     }
 
     function _addAirdropAccount(address _account, uint _amount) private {
-        require(_account != address(0));
-        require(airdropBalances[_account] == 0);
-        require(!airdropReceived[_account]);
+        require(_account != address(0), "Account is zero address");
+        require(airdropBalances[_account] == 0, "Airdrop balance already set");
+        require(!airdropReceived[_account], "Airdrop tokens already received");
         airdropBalances[_account] = _amount;
     }
 
--- test/bounty.test.js
+++ test/bounty.test.js
@@ -69,8 +69,14 @@ contract('Bounty', function (accounts) {
             await bounty.setToken(token.address, {from: bountyOwner});
         });
 
-        it('check balance', async function () {
+        it('transfer', async function () {
+            assert.equal(+(await token.balanceOf(accounts[4])), 0);
             assert.equal(+(await token.balanceOf(bounty.address)), bb);
+
+            await bounty.transfer(accounts[4], vs(10), {from: bountyOwner});
+
+            assert.equal(+(await token.balanceOf(accounts[4])), vs(10));
+            assert.equal(+(await token.balanceOf(bounty.address)), vs(2000000 - 10));
         });
 
         it('add airdrop account', async function () {
@@ -103,8 +109,7 @@ contract('Bounty', function (accounts) {
             assert.equal(await bounty.airdropReceived(accs[0]), true);
             assert.equal(+(await token.balanceOf(accs[1])), 0);
             assert.equal(await bounty.airdropReceived(accs[1]), false);
-            await bounty.receiveTokens({from: accs[0]});
-            
+
             await bounty.receiveTokens({from: accs[1]});
 
             assert.equal(+(await token.balanceOf(accs[0])), amounts[0]);
@@ -112,7 +117,171 @@ contract('Bounty', function (accounts) {
             assert.equal(+(await token.balanceOf(accs[1])), amounts[1]);
             assert.equal(await bounty.airdropReceived(accs[1]), true);
         });
-
-
     });
- });
+
+
+    describe('Requirements and restrictions', async () => {
+        beforeEach('init', async function () {
+            bounty = await BountyContract.new({from: bountyOwner});
+            token = await TokenContract.new(advisors, bounty.address, team, {from: tokenOwner});
+            await bounty.setToken(token.address, {from: bountyOwner});
+        });
+
+        it('add airdrop accounts (different length of arrays)', async function () {
+            accs = [accounts[8], accounts[9]];
+            amounts = [vs(25)];
+
+            try {
+                await bounty.addAirdropAccounts(accs, amounts, {from: bountyOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("The length of the arrays must be equal"));}
+
+            assert.equal(+(await bounty.airdropBalances(accs[0])), 0);
+            assert.equal(await bounty.airdropReceived(accs[0]), false);
+            assert.equal(+(await bounty.airdropBalances(accs[1])), 0);
+            assert.equal(await bounty.airdropReceived(accs[1]), false);
+  
+            accs = [accounts[8], accounts[9]];
+            amounts = [vs(25), vs(20), vs(15)];
+
+            try {
+                await bounty.addAirdropAccounts(accs, amounts, {from: bountyOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("The length of the arrays must be equal"));}
+
+            assert.equal(+(await bounty.airdropBalances(accs[0])), 0);
+            assert.equal(await bounty.airdropReceived(accs[0]), false);
+            assert.equal(+(await bounty.airdropBalances(accs[1])), 0);
+            assert.equal(await bounty.airdropReceived(accs[1]), false);
+
+            accs = [accounts[8], accounts[9]];
+            amounts = [vs(25), vs(20)];
+        });
+
+        it('add airdrop accounts (not by owner)', async function () {
+            try {
+                await bounty.addAirdropAccounts(accs, amounts, {from: tokenOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("Ownable: caller is not the owner"));}
+
+            assert.equal(+(await bounty.airdropBalances(accs[0])), 0);
+            assert.equal(await bounty.airdropReceived(accs[0]), false);
+            assert.equal(+(await bounty.airdropBalances(accs[1])), 0);
+            assert.equal(await bounty.airdropReceived(accs[1]), false);
+        });
+
+        it('add airdrop account (not by owner)', async function () {
+            try {
+                await bounty.addAirdropAccount(accounts[4], vs(25), {from: tokenOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("Ownable: caller is not the owner"));}
+
+            assert.equal(+(await bounty.airdropBalances(accounts[4])), 0);
+            assert.equal(await bounty.airdropReceived(accounts[4]), false);
+        });
+
+        it('add airdrop account (zero address)', async function () {
+            try {
+                await bounty.addAirdropAccount(zeroAddress, vs(25), {from: bountyOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("Account is zero address"));}
+
+            assert.equal(+(await bounty.airdropBalances(accounts[4])), 0);
+            assert.equal(await bounty.airdropReceived(accounts[4]), false);
+            assert.equal(+(await bounty.airdropBalances(zeroAddress)), 0);
+            assert.equal(await bounty.airdropReceived(zeroAddress), false);
+        });
+
+        it('add airdrop account (balance not zero)', async function () {
+            await bounty.addAirdropAccount(accounts[4], vs(25), {from: bountyOwner});
+            
+            assert.equal(+(await bounty.airdropBalances(accounts[4])), vs(25));
+            assert.equal(await bounty.airdropReceived(accounts[4]), false);
+
+            try {
+                await bounty.addAirdropAccount(accounts[4], vs(25), {from: bountyOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("Airdrop balance already set"));}
+
+            assert.equal(+(await bounty.airdropBalances(accounts[4])), vs(25));
+            assert.equal(await bounty.airdropReceived(accounts[4]), false);
+        });
+
+        it('add airdrop account (tokens already received)', async function () {
+            await bounty.addAirdropAccount(accounts[4], vs(25), {from: bountyOwner});
+
+            assert.equal(+(await bounty.airdropBalances(accounts[4])), vs(25));
+            assert.equal(await bounty.airdropReceived(accounts[4]), false);
+
+            await bounty.receiveTokens({from: accounts[4]});
+
+            assert.equal(+(await token.balanceOf(accounts[4])), vs(25));
+            assert.equal(await bounty.airdropReceived(accounts[4]), true);
+
+            try {
+                await bounty.addAirdropAccount(accounts[4], vs(25), {from: bountyOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("Airdrop tokens already received"));}
+
+            assert.equal(+(await bounty.airdropBalances(accounts[4])), 0);
+            assert.equal(await bounty.airdropReceived(accounts[4]), true);
+        });
+
+        it('receive tokens (tokens already received)', async function () {
+            await bounty.addAirdropAccount(accounts[4], vs(25), {from: bountyOwner});
+
+            assert.equal(+(await bounty.airdropBalances(accounts[4])), vs(25));
+            assert.equal(await bounty.airdropReceived(accounts[4]), false);
+
+            await bounty.receiveTokens({from: accounts[4]});
+
+            assert.equal(+(await bounty.airdropBalances(accounts[4])), 0);
+            assert.equal(+(await token.balanceOf(accounts[4])), vs(25));
+            assert.equal(await bounty.airdropReceived(accounts[4]), true);
+
+            try {
+                await bounty.receiveTokens({from: accounts[4]});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("Airdrop tokens already received"));}
+
+            assert.equal(+(await bounty.airdropBalances(accounts[4])), 0);
+            assert.equal(+(await token.balanceOf(accounts[4])), vs(25));
+            assert.equal(await bounty.airdropReceived(accounts[4]), true);
+        });
+
+        it('set token address (not by owner)', async function () {
+            assert.equal(await bounty.token(), token.address);
+            try {
+                await bounty.setToken(token.address, {from: tokenOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("Ownable: caller is not the owner"));}
+            assert.equal(await bounty.token(), token.address);
+        });
+
+        it('transfer (not by owner)', async function () {
+            assert.equal(+(await token.balanceOf(accounts[4])), 0);
+            assert.equal(+(await token.balanceOf(bounty.address)), bb);
+
+            try {
+                await bounty.transfer(accounts[4], vs(10), {from: tokenOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("Ownable: caller is not the owner"));}
+
+            assert.equal(+(await token.balanceOf(accounts[4])), 0);
+            assert.equal(+(await token.balanceOf(bounty.address)), vs(2000000));
+        });
+
+        it('transfer (more than have)', async function () {
+            assert.equal(+(await token.balanceOf(accounts[4])), 0);
+            assert.equal(+(await token.balanceOf(bounty.address)), bb);
+
+            try {
+                await bounty.transfer(accounts[4], vs(2000001), {from: bountyOwner});
+                throw "Fail!\n Exception must be thrown before";
+            } catch (error) {assert(error.message.includes("SafeMath: subtraction overflow"));}
+
+            assert.equal(+(await token.balanceOf(accounts[4])), 0);
+            assert.equal(+(await token.balanceOf(bounty.address)), vs(2000000));
+        });
+    });
+});
